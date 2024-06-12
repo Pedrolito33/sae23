f0050b51c7cd2039f4849618cc1c4f66d2befa5d
Pedrolito33 <73652335+Pedrolito33@users.noreply.github.com>
2024-06-12 11:44:28 +0200

    Formulaire de connexion

---
+++ login_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST" action="login.php">
+        Nom utilisateur: <input type="text" name="username" required><br>
+        Mot de passe: <input type="password" name="password" required><br>
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
